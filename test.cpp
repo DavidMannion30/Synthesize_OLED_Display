a2afa462dbe518a8bcb5f5d9ad8670a5dc854e35
DavidMannion30 <damannio@tcd.ie>
2026-03-20 15:19:48 +0000

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -28,13 +28,38 @@
 
 // void loop() {}
 
-#include <U8g2lib.h>
+// #include <U8g2lib.h>
 
-U8G2_ST7920_128X64_F_SW_SPI u8g2(
+// U8G2_ST7920_128X64_F_SW_SPI u8g2(
+//   U8G2_R0,
+//   /* clock=*/ 13,
+//   /* data=*/ 11,
+//   /* CS=*/ 10,
+//   /* reset=*/ 8
+// );
+
+// void setup() {
+//   u8g2.begin();
+// }
+
+// void loop() {
+//   u8g2.clearBuffer();
+//   u8g2.setFont(u8g2_font_ncenB08_tr);
+//   u8g2.drawStr(0, 20, "Hello!");
+//   u8g2.sendBuffer();
+//   delay(1000);
+// }
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+
+// clock = 13, data = 11 handled by hardware SPI
+// cs = 10, dc = 9, reset = 8
+U8G2_SSD1309_128X64_NONAME0_F_4W_HW_SPI u8g2(
   U8G2_R0,
-  /* clock=*/ 13,
-  /* data=*/ 11,
-  /* CS=*/ 10,
+  /* cs=*/ 10,
+  /* dc=*/ 9,
   /* reset=*/ 8
 );
 
@@ -45,7 +70,8 @@ void setup() {
 void loop() {
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_ncenB08_tr);
-  u8g2.drawStr(0, 20, "Hello!");
+  u8g2.drawStr(0, 24, "Hello");
+  u8g2.drawStr(0, 48, "SSD1309");
   u8g2.sendBuffer();
   delay(1000);
 }
