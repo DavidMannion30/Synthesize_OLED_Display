02ca132993905bc7711a3e705ffd6396f3ece9c4
DavidMannion30 <damannio@tcd.ie>
2026-03-20 14:56:06 +0000

    Refactor to use LiquidCrystal instead of OLED
    Comment out OLED display code and add LCD initialization.

--- test.cpp
+++ test.cpp
@@ -1,29 +1,41 @@
-#include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+// #include <SPI.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+// #define SCREEN_WIDTH 128
+// #define SCREEN_HEIGHT 64
 
-#define OLED_MOSI 11
-#define OLED_CLK  13
-#define OLED_DC   9
-#define OLED_CS   10
-#define OLED_RESET 8
+// #define OLED_MOSI 11
+// #define OLED_CLK  13
+// #define OLED_DC   9
+// #define OLED_CS   10
+// #define OLED_RESET 8
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
-  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
+//   OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+// void setup() {
+//   if (!display.begin(SSD1306_SWITCHCAPVCC)) {
+//     while (true);
+//   }
+
+//   display.clearDisplay();
+//   display.setTextSize(2);
+//   display.setCursor(10, 20);
+//   display.println("OLED OK");
+//   display.display();
+// }
+
+// void loop() {}
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
-  if (!display.begin(SSD1306_SWITCHCAPVCC)) {
-    while (true);
-  }
-
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setCursor(10, 20);
-  display.println("OLED OK");
-  display.display();
+  lcd.begin(16, 2);
+  lcd.print("LCD OK");
 }
 
-void loop() {}
+void loop() {
+}
